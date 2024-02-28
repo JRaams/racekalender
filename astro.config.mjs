f0484c1f46f26d8a98ea21f7dca30d8ebc507cdd
JRaams <47593569+JRaams@users.noreply.github.com>
2024-02-28 21:19:03 +0100

    purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
-import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
+import purgecss from "astro-purgecss";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), icon()],
+  build: {
+    inlineStylesheets: "auto",
+  },
+  integrations: [
+    svelte(),
+    icon(),
+    purgecss({
+      safelist: {
+        standard: [":hover", ":focus", ":where", ":is", "button", ".past"]
+      },
+    }),
+  ],
 });
