bb2c2ac25277fcf4c1ebbfa7ae57815d2e621ae1
JRaams <47593569+JRaams@users.noreply.github.com>
2024-02-29 21:36:51 +0100

    setup language switcher

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,5 +16,5 @@ export default defineConfig({
         standard: [":hover", ":focus", ":where", ":is", "button", ".past"]
       },
     }),
-  ],
+  ]
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languages = {
+  'en': {
+    label: 'English',
+    flagIconName: 'flagpack:gb-ukm'
+  },
+  'nl': {
+    label: 'Nederlands',
+    flagIconName: 'flagpack:nl'
+  }
+}
+
+export const defaultLang = "nl";
+
+export const ui = {
+  en: {
+    "nav.title": "Racecalendar 2024"
+  },
+  nl: {
+    "nav.title": "Racekalender 2024"
+  },
+} as const;
