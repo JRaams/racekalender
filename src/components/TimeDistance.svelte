b46d0606957099d033bb781a8996862a475a6dbd
JRaams <47593569+JRaams@users.noreply.github.com>
2024-02-29 21:58:44 +0100

    dutch translatiuons

--- src/components/TimeDistance.svelte
+++ src/components/TimeDistance.svelte
@@ -2,14 +2,19 @@
     import dayjs from 'dayjs';
     import 'dayjs/locale/nl';
     import relativeTime from 'dayjs/plugin/relativeTime';
-    dayjs.extend(relativeTime);
-    dayjs.locale('nl');
-    
+    import type { Language } from '~/i18n/ui';
+    import { useTranslations } from '~/i18n/utils';
+
+    export let lang: Language; 
     export let timestamp: number;
+
+    const t = useTranslations(lang);
+    dayjs.extend(relativeTime);
+    dayjs.locale(lang);
     
     const date = dayjs(timestamp);
     const today = date.isSame(new Date(), 'days');
-    const dateDistance = today ? 'Vandaag!' : date.fromNow()
+    const dateDistance = today ? t('time.today') : date.fromNow()
 </script>
 
 <small>{dateDistance}</small>
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,13 +9,25 @@ export const languages = {
   }
 }
 
-export const defaultLang = "nl";
+export type Language = keyof typeof languages;
+
+export const defaultLang = "nl" satisfies Language;
 
 export const ui = {
   en: {
-    "nav.title": "Racecalendar 2024"
+    "nav.title": "Racecalendar 2024",
+    "list.table.round": "Round",
+    "list.table.name": "Name",
+    "list.table.date": "Date",
+    "list.table.circuit": "Circuit",
+    "time.today": "Today!"
   },
   nl: {
-    "nav.title": "Racekalender 2024"
+    "nav.title": "Racekalender 2024",
+    "list.table.round": "Ronde",
+    "list.table.name": "Naam",
+    "list.table.date": "Datum",
+    "list.table.circuit": "Circuit",
+    "time.today": "Vandaag!"
   },
 } as const;
