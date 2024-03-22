b785b6a54c1858a9657162a238de5fc43147c1ed
JRaams <47593569+JRaams@users.noreply.github.com>
2024-03-22 20:15:03 +0100

    sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
+import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import icon from 'astro-icon';
 import purgecss from 'astro-purgecss';
+import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://racekalender.pages.dev',
   build: {
     inlineStylesheets: 'auto',
   },
@@ -16,5 +19,7 @@ export default defineConfig({
         standard: [':hover', ':focus', ':where', ':is', 'button', '.past'],
       },
     }),
+    robotsTxt(),
+    sitemap(),
   ],
 });
